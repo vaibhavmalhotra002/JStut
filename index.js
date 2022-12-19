ecfe0c607c332e078c5d71410c4fd75646cf2e3f
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-12-19 21:17:37 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 console.log('Hello Javascript');
-let name ='Vaibhav';
-console.log(name);
+// let name ='Vaibhav';
+// console.log(name);
 
-const interestRate=0.3;
+ interestRate=0.3;
 interestRate=1;
 console.log(interestRate);
+
+//obj
+let person ={
+   name :'Vaibhav',
+   age : 20
+};
+//dot notation
+person.name='vaibhavi;'
+
+
+console.log(person);
+
+//
